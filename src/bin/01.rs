3f3a8ada0ffdb177b4154f20e9f61a1d649732fe
Sam <sam+github@gizm0.dev>
2024-12-01 23:10:59 -0500

    Day 1 Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -2,6 +2,7 @@ use adv_code_2024::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -23,16 +24,23 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn parse_int(val: &str) -> u32 {
-        u32::from_str_radix(val, 10).unwrap()
+    fn parse_int(val: &str) -> usize {
+        usize::from_str_radix(val, 10).unwrap()
     }
 
-    fn part1<R: BufRead>(reader: R) -> Result<u32> {
-        let parsed_lines = reader.lines().flatten().map(|line| {
-            let (left, right) = line.split_at(line.find("   ").unwrap());
-            (parse_int(left), parse_int(&right[3..]))
-        });
-        let (mut left_list, mut right_list): (Vec<_>, Vec<_>) = parsed_lines.unzip();
+    fn parse_file<R: BufRead>(reader: R) -> (Vec<usize>, Vec<usize>) {
+        reader
+            .lines()
+            .flatten()
+            .map(|line| {
+                let (left, right) = line.split_at(line.find("   ").unwrap());
+                (parse_int(left), parse_int(&right[3..]))
+            })
+            .unzip()
+    }
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let (mut left_list, mut right_list) = parse_file(reader);
         left_list.sort();
         right_list.sort();
         let answer = left_list
@@ -51,17 +59,29 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let (left_list, right_list) = parse_file(reader);
+        let mut similarity_score = 0;
+        let mut right_counts: HashMap<usize, usize> = HashMap::new();
+
+        for element in right_list {
+            right_counts.insert(element, *right_counts.get(&element).unwrap_or(&0) + 1);
+        }
+
+        for element in left_list {
+            similarity_score += right_counts.get(&element).unwrap_or(&0) * element;
+        }
+
+        Ok(similarity_score)
+    }
+
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
